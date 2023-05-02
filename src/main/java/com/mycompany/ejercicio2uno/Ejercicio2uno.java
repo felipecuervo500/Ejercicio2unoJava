4586d5ea12ea2ec17ffc4ae5f15f1e97fd837403
Felipe Cuervo Romero <felipecuervo500@gmail.com>
2023-05-02 16:41:15 -0500

    Primer commit

---
+++ src/main/java/com/mycompany/ejercicio2uno/Ejercicio2uno.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.ejercicio2uno;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Ejercicio2uno {
+
+    public static void main(String[] args) {
+        byte n = 127;
+        short s = 32767;
+        int p = 8000000;
+        long l = 450000L;
+        
+        float a = 8.5f;
+        double b = 10.99d;
+        
+        boolean opcion = true;
+        
+        char c = 'c';
+        String d = "Holamundo";
+        
+        System.out.println(n);
+        System.out.println(s);
+        System.out.println(p);
+        System.out.println(l);
+        
+        System.out.println(a);
+        System.out.println(b);
+        
+        System.out.println(opcion);
+        
+        System.out.println(c);
+        System.out.println(d);
+        
+    }
+}
